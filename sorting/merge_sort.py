7eafe8d2b44c0a622552b5d78aa2f07719c8ed4c
chaewonjeong <chaewonjeong.dev@gmail.com>
2023-05-06 06:56:20 +0900

    20230506 Study

---
+++ sorting/merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+
+
+list = [3, 56, 123, 3, 55, 33, 4, 12, 56, 44, 427, 45, 32, 8, 76, 10]
+print(list)
+merge_sort(list)
+print(list)
--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,32 +1,31 @@
 def partition(arr, p, r):
     i = p-1
     j = p
-    x = arr[r]
+    pivot = arr[r]
 
     while j <= r-1:
-        if arr[j] >= x:
+        if arr[j] >= pivot:
             j += 1
         else:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
             j += 1
-    arr[i+1], arr[r] = arr[r], arr[i + 1]
-
+    arr[r], arr[i+1] = arr[i+1], arr[r]
     return i+1
 
 
 def qsort(arr, p, r):
     if p <= r:
         q = partition(arr, p, r)
-        qsort(arr, p, q-1)
-        qsort(arr, q+1, r)
+        qsort = partition(arr, p, q-1)
+        qsort = partition(arr, q+1, r)
 
 
-def quickSort(arr):
+def quick_sort(arr):
     qsort(arr, 0, len(arr)-1)
 
 
 list = [3, 56, 123, 3, 55, 33, 4, 12, 56, 44, 427, 45, 32, 8, 76, 10]
 print(list)
-quickSort(list)
+quick_sort(list)
 print(list)
