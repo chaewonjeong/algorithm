c66878e92fb45d85efdb242ca30f2001fd559ec7
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-01-30 22:23:16 +0900

    [level 2] Title: 올바른 괄호, Time: 25.53 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(stack.isEmpty()){
+                if(c == ')'){
+                    return false;
+                }
+                stack.push(c);
+                continue;
+            }
+            
+            if(stack.peek() == c){
+                stack.push(c);
+            } else if(stack.peek() != c){
+                stack.pop();
+            }
+            
+        }
+
+        return stack.isEmpty();
+    }
+}
