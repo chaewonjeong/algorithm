49c6586e499d638d75ce67d40bd4facfcd35fb78
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-02-05 01:27:56 +0900

    [level 2] Title: 주식가격, Time: 40.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,22 +2,24 @@ import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] prices) {
-        Stack<Integer> stack = new Stack();
-        int n = prices.length;
-        int[] answer = new int[n];
-        stack.push(0);
+        int[] answer = new int[prices.length];
         
-        for(int i = 0; i<n; i++){
+        // 가격이 떨어지는 시점 -> 인접한 원소에대한 처리 -> 스택인가???
+        // 스택을 다룰때 뭔가 직접 값을 넣을까?? 아니면 인덱스를 넣을까?? 스택에 어떤 value를 주지??
+        
+        Stack<Integer> stack = new Stack();
+        int n = prices.length - 1;
+        
+        for(int i = 0; i < prices.length; i++){
             while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
-                int j = stack.pop();
-                answer[j] = i-j;
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
             stack.push(i);
         }
-        
         while(!stack.isEmpty()){
-            int j = stack.pop();
-            answer[j] = n - j - 1;
+            int i = stack.pop();
+            answer[i] = n - i; 
         }
         
         
