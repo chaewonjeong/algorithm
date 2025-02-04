4b2d03c0675931b98e59feaffb127c598ea1f074
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-02-04 23:37:03 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 312 ms, Memory: 19832 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        Queue<Integer> deque = new ArrayDeque<>();
+
+        // 1부터 N까지 큐에 추가
+        for (int i = 1; i <= n; i++) {
+            deque.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (!deque.isEmpty()) {
+            for (int i = 0; i < k - 1; i++) {
+                deque.add(deque.poll());
+            }
+            sb.append(deque.poll());
+
+            if (!deque.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append(">");
+        System.out.println(sb.toString());
+    }
+}
