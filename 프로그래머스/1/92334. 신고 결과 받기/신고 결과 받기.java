61f23e76b839d3f0c51fa3104506c94bc0a40cb3
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-02-15 13:57:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 208.96 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        HashMap<String, Integer> idMap = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> repotMap = new HashMap<>(); // 신고 당한 사람이 key, 신고한 사람이 배열로 value
+        
+        for(int i = 0; i < id_list.length; i++){
+            String id = id_list[i]; 
+            idMap.put(id, i);
+        }
+        
+        for(String r : report){
+            String[] rep = r.split(" ");
+            
+            int user = idMap.get(rep[0]); // 신고한 사람 id index
+            
+            int repot = idMap.get(rep[1]); // 신고당한 사람 id index
+            
+            if(!repotMap.containsKey(repot)){
+                repotMap.put(repot, new HashSet<>());
+            }
+            repotMap.get(repot).add(user);
+        }
+        
+        Map<Integer, HashSet<Integer>> filteredMap = repotMap.entrySet().stream()
+                .filter(entry -> entry.getValue().size() >= k) 
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        
+        
+        filteredMap.forEach((key, value) -> {
+            Integer[] tmp = value.toArray(Integer[]::new);
+            for(int i : tmp){
+                answer[i]++;
+            }}
+        );
+        
+        
+        
+        return answer;
+    }
+}
