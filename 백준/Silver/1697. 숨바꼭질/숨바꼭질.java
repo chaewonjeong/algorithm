d5b091a83a496b8071856c632e3ec36d7c2491fb
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-03-08 16:14:44 +0900

    [Silver I] Title: 숨바꼭질, Time: 220 ms, Memory: 23640 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 수빈 위치
+        int k = sc.nextInt(); // 동생 위치
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[100001];
+        queue.add(new int[]{n,0});
+        visited[n] = true;
+
+        while(!queue.isEmpty()){
+            int arr[] = queue.poll();
+            int x = arr[0];
+            int level = arr[1];
+            if(x == k){
+                System.out.println(level);
+                return;
+            }
+            int[] next = {x * 2, x + 1, x - 1};
+
+            for(int nxt : next){
+                if(nxt < 0 || nxt > 100000 || visited[nxt]) continue;
+                visited[nxt] = true;
+                queue.add(new int[]{nxt,level+1});
+
+            }
+
+
+        }
+    }
+}
