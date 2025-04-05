078459878b9b913790c673cfddbc1f83bd08a0c5
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-04-05 10:28:36 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -7,16 +7,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        long[][] dp = new long [91][2];
-        dp[1][0] = 1;
-        dp[1][1] = 0;
-        dp[2][0] = 1;
-        dp[2][1] = 0;
+        long[] dp = new long [91];
+        dp[1] = 1;
+        dp[2] = 1;
 
         for(int i = 3; i <= n; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            dp[i][1] = dp[i-1][0];
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        System.out.println(dp[n][0] + dp[n][1]);
+        System.out.println(dp[n]);
     }
 }
