15ad9f701981f23a4c40794c725948d56023bcba
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-04-29 00:42:21 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 16032 ms, Memory: 102584 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        int[] freq = new int[2_000_001];
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            num += 1_000_000;
+            freq[num]++;
+        }
+
+        for(int i = 0 ; i <= 2_000_000; i++) {
+            while(freq[i] > 0) {
+                answer.append(i - 1_000_000).append("\n");
+                freq[i]--;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
