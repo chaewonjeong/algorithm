fc755e5bf89781683cc96df5148200a190e9eeca
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-04-29 15:57:40 +0900

    [Silver IV] Title: 동전 0, Time: 284 ms, Memory: 18336 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<Integer>();
+
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(br.readLine());
+            set.add(v);
+        }
+        int count = 0;
+
+        while(k > 0){
+            int coin = set.floor(k);
+            count++;
+            k -= coin;
+        }
+        
+        System.out.println(count);
+
+    }
+}
