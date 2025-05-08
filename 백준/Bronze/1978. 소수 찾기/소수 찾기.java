90813ac5492f2603548965640142c5046462c909
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-05-08 12:15:50 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (isPrime(num)) count++;
+        }
+        System.out.println(count);
+    }
+}
