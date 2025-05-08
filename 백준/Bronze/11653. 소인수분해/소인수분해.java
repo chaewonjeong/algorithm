cc80b8c2169dc1b830764800c86222d4b9cfcd17
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-05-08 13:05:42 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -6,16 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         StringBuilder answer = new StringBuilder();
-        for (int i = 2; i * i <= n; i++) {
-            while (n % i == 0) {
+        int i = 2;
+        while (i * i <= n) {
+            if(n % i == 0) {
                 answer.append(i).append("\n");
                 n /= i;
+            } else {
+                i++;
             }
         }
         if(n != 1) answer.append(n).append("\n");
-
         System.out.println(answer);
     }
 }
