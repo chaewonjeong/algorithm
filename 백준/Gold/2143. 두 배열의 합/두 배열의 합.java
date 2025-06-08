a113bd05746c1c1f279510a02844b689ae297f2e
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-06-09 02:42:10 +0900

    [Gold III] Title: 두 배열의 합, Time: 992 ms, Memory: 70880 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long T = Long.parseLong(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        long[] prefixA = new long[n];
+        prefixA[0] = A[0];
+        for(int i = 1; i < n; i++) {
+            prefixA[i] = prefixA[i - 1] + A[i];
+        }
+
+
+        int m = Integer.parseInt(br.readLine());
+        int[] B = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        long[] prefixB = new long[m];
+        prefixB[0] = B[0];
+        for(int i = 1; i < m; i++) {
+            prefixB[i] = prefixB[i - 1] + B[i];
+        }
+
+        ArrayList<Long> subA = new ArrayList<>();
+        ArrayList<Long> subB = new ArrayList<>();
+
+        for(int i = -1; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(i == -1){
+                    subA.add(prefixA[j]);
+                } else{
+                    subA.add(prefixA[j] - prefixA[i]);
+                }
+            }
+        }
+
+        for (int i = -1; i < m - 1; i++) {
+            for(int j = i + 1; j < m; j++) {
+                if(i == -1){
+                    subB.add(prefixB[j]);
+                } else {
+                    subB.add(prefixB[j] - prefixB[i]);
+                }
+            }
+        }
+
+        long[] subAList = subA.stream().sorted().mapToLong(Long::longValue).toArray();
+        long[] subBList = subB.stream().sorted().mapToLong(Long::longValue).toArray();
+        long count = 0;
+        for (long l : subAList) {
+            long target = T - l;
+            int lb = lowerBound(subBList, target);
+            int ub = upperBound(subBList, target);
+            if (ub >= lb) {
+                count += ub - lb + 1;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+    static int upperBound(long[] arr, long target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    static int lowerBound(long[] arr, long target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+}
