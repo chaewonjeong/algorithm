f6070a77ff828076f17a7334f229b3fdb6ae8a7f
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-08-06 00:54:34 +0900

    [Silver I] Title: 그림, Time: 436 ms, Memory: 48180 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,62 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    // 도화지에 그려진 그림의 개수와 그 그림들 중 가장 큰 그림을 출력할 것
-    public static void main(String[] args) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        Scanner sc = new Scanner(System.in);
 
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int max = Integer.MIN_VALUE;
 
-        int countPictures = 0;
-        int max = 0;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[][] dists = {{1,0}, {0,1}, {-1,0}, {0,-1}};
 
-        int n = sc.nextInt(); // 그림의 세로 크기
-        int m = sc.nextInt(); // 그림의 가로 크기
 
-        int[][] maps = new int[n][m];
-        int[][] visited = new int[n][m];
+    static int bfs(int startR, int startC) {
+        int count = 0;
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                maps[i][j] = sc.nextInt();
-            }
-        }
+        queue.add(new int[]{startR, startC});
+        visited[startR][startC] = true;
+        count++;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
 
-                if(maps[i][j] == 1 && visited[i][j] == 0) {
-                    int count = 1; // 첫 칸도 한칸으로 세야함
-                    visited[i][j] = 1;
+            for (int[] dist: dists) {
+                int newR = cur[0] + dist[0];
+                int newC = cur[1] + dist[1];
 
-                    queue.add(new int[]{i, j});
 
-                    while(!queue.isEmpty()) {
-                        int[] current = queue.poll();
-                        int row = current[0];
-                        int col = current[1];
-
-                        for(int[] dir : dirs) {
-                            int newRow = row + dir[0];
-                            int newCol = col + dir[1];
-
-                            if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m
-                            && visited[newRow][newCol] == 0 && maps[newRow][newCol] == 1) {
-                                visited[newRow][newCol] = 1;
-                                queue.add(new int[]{newRow, newCol});
-                                count++;
-                            }
-                        }
+                if (newR >= 0 && newR < board.length
+                        && newC >= 0 && newC < board[0].length) {
+                    if(board[newR][newC] == 1 && visited[newR][newC] == false) {
+                        queue.add(new int[]{newR, newC});
+                        visited[newR][newC] = true;
+                        count++;
                     }
-                    countPictures++;
-                    max = Math.max(max, count);
                 }
             }
         }
-        System.out.println(countPictures);
-        System.out.println(max);
+
+
+        return count;
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 0;
+        board = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1 &&  visited[i][j] == false) {
+                    count++;
+                    max = Math.max(bfs(i,j), max);
+                }
+            }
+        }
+
+        System.out.println(count);
+        if(max == Integer.MIN_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(max);
+        }
     }
 }
