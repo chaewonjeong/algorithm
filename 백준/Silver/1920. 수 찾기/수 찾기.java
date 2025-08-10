77f6af13e3c95d2102b151cc3112811ab1409e43
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-08-10 21:10:44 +0900

    [Silver IV] Title: 수 찾기, Time: 536 ms, Memory: 43352 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,42 +5,54 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int search(int[] nums,int left, int right, int target) {
-        if(left > right) return 0;
-        int center = (left + right) / 2;
-        if(nums[center] == target) return 1;
-        else if( nums[center] > target) {
-            return search(nums, left, center - 1, target);
-        }else {
-            return search(nums, center + 1, right, target);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
+
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(nums);
+
+        Arrays.sort(arr);
 
         int m = Integer.parseInt(br.readLine());
-
-        int[] targets = new int[m];
+        int[] target = new int[m];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            targets[i] = Integer.parseInt(st.nextToken());
+            target[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i : targets) {
-            int ans = search(nums,0, nums.length-1, i);
-            answer.append(ans).append("\n");
+
+        for(int tar : target){
+            int left = 0;
+            int right = n - 1;
+            int flag = 0;
+            while (left <= right) {
+                int mid = (left + right)/2;
+
+                if (arr[mid] < tar) {
+                    left = mid + 1;
+                } else if (arr[mid] > tar) {
+                    right = mid - 1;
+                } else {
+                    sb.append("1");
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0){
+                sb.append("0");
+            }
+            sb.append('\n');
         }
 
-        System.out.println(answer.toString());
+        System.out.println(sb);
+
+
+
+
     }
 }
