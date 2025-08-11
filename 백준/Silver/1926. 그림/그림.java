8d311b8d01aa77d7c5adbffa579d0c5028ccdb2e
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-08-12 03:09:38 +0900

    [Silver I] Title: 그림, Time: 376 ms, Memory: 53572 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,81 +2,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int max = Integer.MIN_VALUE;
-
-    static int[][] board;
-    static boolean[][] visited;
-    static int[][] dists = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-
-
-    static int bfs(int startR, int startC) {
-        int count = 0;
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-
-        queue.add(new int[]{startR, startC});
-        visited[startR][startC] = true;
-        count++;
-
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-
-            for (int[] dist: dists) {
-                int newR = cur[0] + dist[0];
-                int newC = cur[1] + dist[1];
-
-
-                if (newR >= 0 && newR < board.length
-                        && newC >= 0 && newC < board[0].length) {
-                    if(board[newR][newC] == 1 && visited[newR][newC] == false) {
-                        queue.add(new int[]{newR, newC});
-                        visited[newR][newC] = true;
-                        count++;
-                    }
-                }
-            }
-        }
-
-
-        return count;
-
-    }
-
+    static int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int count = 0;
-        board = new int[n][m];
-        visited = new boolean[n][m];
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int n = Integer.parseInt(st.nextToken());
+       int m = Integer.parseInt(st.nextToken());
 
+       int[][] board = new int[n][m];
+       int[][] visited = new int[n][m];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 1 &&  visited[i][j] == false) {
-                    count++;
-                    max = Math.max(bfs(i,j), max);
-                }
-            }
-        }
+       for(int i = 0; i < n; i++){
+           st = new StringTokenizer(br.readLine());
+           for(int j = 0; j < m; j++){
+               board[i][j] = Integer.parseInt(st.nextToken());
+           }
+       }
+
+       int biggest = 0;
+       int count = 0;
+
+       for(int i = 0; i < n; i++){
+           for(int j = 0; j < m; j++){
+               if(board[i][j] == 1 && visited[i][j] == 0){
+                   Queue<int[]> queue = new ArrayDeque<>();
+                   queue.add(new int[]{i, j});
+                   int area = 1;
+                   visited[i][j] = 1;
+                   while(!queue.isEmpty()){
+                       int[] cur = queue.poll();
+                       int row =  cur[0];
+                       int col = cur[1];
+
+                       for(int[] dir : dirs){
+                           int newRow = row + dir[0];
+                           int newCol = col + dir[1];
+
+                           if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m){
+                               if(board[newRow][newCol] == 1 && visited[newRow][newCol] == 0){
+                                   queue.add(new int[]{newRow, newCol});
+                                   area++;
+                                   visited[newRow][newCol] = 1;
+                               }
+                           }
+
+                       }
+                   }
+                   count++;
+                   biggest = Math.max(area, biggest);
+
+               }
+
+           }
+       }
 
         System.out.println(count);
-        if(max == Integer.MIN_VALUE) {
-            System.out.println(0);
-        } else {
-            System.out.println(max);
-        }
+        System.out.println(biggest);
+
+
     }
 }
