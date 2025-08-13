f6856a2220a199aac5ac2c52ee53ff86aeefb8b9
CHAEWON JEONG <69059590+chaewonjeong@users.noreply.github.com>
2025-08-14 02:10:19 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        sc.nextLine();
-        int[][] maps = new int[n][m];
+    static int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n =  Integer.parseInt(st.nextToken());
+        int m =  Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[n][m];
         int[][] visited = new int[n][m];
 
-        Queue<int[]> queue = new ArrayDeque<>();
 
-        // 각각의 수들이 붙어서 입력으로 주어진다고 했음
-        // readLine으로 읽어서 charAt으로 넣자..;
-        for(int i = 0; i < n; i++) {
-            String str = sc.nextLine();
-            for(int j = 0; j < m; j++) {
-                maps[i][j] = str.charAt(j) - '0';
+
+        for (int i = 0; i < n; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+
+                board[i][j] = chars[j] - '0';
             }
         }
 
-        queue.add(new int[]{0, 0});
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0});
         visited[0][0] = 1;
 
-        while(!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int row = current[0];
-            int col = current[1];
 
-            if(row == n - 1 && col == m - 1) {
-                System.out.println(visited[row][col]);
-                return;
-            }
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int row = cur[0];
+            int col = cur[1];
 
             for(int[] dir : dirs){
                 int newRow = row + dir[0];
                 int newCol = col + dir[1];
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m
-                && visited[newRow][newCol] == 0 && maps[newRow][newCol] == 1) {
-                    visited[newRow][newCol] = visited[row][col] + 1;
-                    queue.add(new int[]{newRow, newCol});
+
+                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m){
+                    if(board[newRow][newCol] == 1 && visited[newRow][newCol] == 0){
+                        visited[newRow][newCol] = visited[row][col] + 1;
+                        queue.add(new int[]{newRow, newCol});
+                    }
                 }
+
+
             }
         }
-        return;
+
+        System.out.println(visited[n - 1 ][m - 1]);
+
 
     }
 }
